0a68185cd85dcbf868b28a15f45bdfa91832feaf
sbohmann <mail@sebastian-bohmann.com>
2017-10-06 10:11:43 +0200

    Even faster now without a copy

--- src/main/java/at/or/vogt/looplambda/SplitStream.java
+++ src/main/java/at/or/vogt/looplambda/SplitStream.java
@@ -1,5 +1,6 @@
 package at.or.vogt.looplambda;
 
+import java.util.Arrays;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
@@ -7,18 +8,33 @@ import java.util.stream.Stream;
  *
  */
 class SplitStream extends CodePrinter {
-    private final static Pattern portableNewline = Pattern.compile("\\r?\\n");
+    private static final Pattern portableNewline = Pattern.compile("\\r?\\n");
+    private static final Pattern compoundNewline = Pattern.compile("\\r\\n");
+    private static final Pattern simpleNewline = Pattern.compile("\\n");
     
     private long numberOfBytes;
 
     @Override
     public void printCode(final String code) {
-        // Arrays.asList(code.split("\\r?\\n")).stream().forEach(System.out::println);
-        // Stream.of(code.split("\\r?\\n")).forEach(System.out::println);
-        // System.out.println(code.replaceAll("\\r\\n", "\\n"));
-        //Arrays.asList(code.replaceAll("\\\\r\\\\n", "\\n").split("\\n")).forEach(System.out::println);
-    
-        Stream.of(portableNewline.split(code)).forEach(this::count);
+        // Arrays.asList(code.split("\\r?\\n")).stream().forEach(this::count);
+        // Stream.of(code.split("\\r?\\n")).forEach(this::count);
+        // count(code.replaceAll("\\r\\n", "\\n"));
+        
+        //Arrays.asList(simpleNewline.split(code.replace("\r\n", "\n"))).forEach(this::count);
+        
+//        for (String intermediate : compoundNewline.split(code))
+//        {
+//            for (String line : simpleNewline.split(intermediate))
+//            {
+//                count(line);
+//            }
+//        }
+        
+        Stream.of(compoundNewline.split(code))
+            .flatMap(intermediate -> Stream.of(simpleNewline.split(intermediate)))
+            .forEach(this::count);
+            
+        //Stream.of(portableNewline.split(code)).forEach(this::count);
+        //Arrays.asList(portableNewline.split(code)).forEach(this::count);
     }
-
 }
